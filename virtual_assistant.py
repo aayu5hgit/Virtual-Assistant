625c090e9c7960cdaab5562d3fc5e7c6cbbaea93
AAYUSH TALREJA <amtalreja02@gmail.com>
2022-04-28 22:12:10 +0530

    Streak Resume

--- virtual_assistant.py
+++ virtual_assistant.py
@@ -36,7 +36,7 @@ def takeCommand():
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
+        print("Listening...\nTry Saying 'Sachin Tendulkar Wikipedia'")
         r.pause_threshold = 1
         audio = r.listen(source)
 
@@ -47,7 +47,7 @@ def takeCommand():
 
     except Exception as e:
         # print(e)
-        print("Say that again please...")
+        print("Not so clear, say that again please...")
         return "None"
     return query
 
